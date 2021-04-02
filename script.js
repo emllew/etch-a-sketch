dbf030ad6cc244c1db17ee0212b685b6758a0804
Emily <emilyllewelyn@gmail.com>
2021-04-02 15:16:48 +0100

    box resizes within the container

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const button = document.querySelector('button');
 let rows = (button.onclick = prompt('How big will this grid be?'));
 let columns = rows;
 function makeGrid(rows, cols) {
-  container.setAttribute('style', `--grid-rows: ${rows}`);
+  container.setAttribute(
+    'style',
+    ` grid-template-rows: repeat(var(${rows}), 1fr);`
+  );
   container.setAttribute('style', `--grid-cols: ${cols}`);
   for (let i = 0; i < rows * cols; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
     container.appendChild(box);
-    box.innerText = i + 1;
+
     box.addEventListener('mouseover', changecolor);
 
     function changecolor() {
